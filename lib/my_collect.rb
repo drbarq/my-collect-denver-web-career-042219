39794965d6b43d10c2bb923e1453fae1363da740
Joe Tustin <j.tustin@gmail.com>
2019-03-15 21:47:49 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -6,11 +6,16 @@ def my_collect(collection)
   my_array = Array.new
   while i < collection.length do
      yield(collection)
-     my_array << collection
-    i = i + 1
+     my_array << collection[i]
+     i = i + 1
   end
   return my_array
 end
 
 
 ## languages = ['ruby', 'javascript', 'python', 'objective-c']
+=begin
+my_collect(languages) do |language|
+ puts language
+end
+=end 
