74e721ae5ce5e47fbb420a598a40c9a01e919db9
Joe Tustin <j.tustin@gmail.com>
2019-03-15 21:54:21 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,13 +3,12 @@
 
 def my_collect(collection)
   i = 0
-  return my_array
+  my_array = Array.new
   while i < collection.length do
-    my_array = Array.new
      my_array << yield(collection[i])
      i = i + 1
   end
-
+  return my_array
 end
 
 
