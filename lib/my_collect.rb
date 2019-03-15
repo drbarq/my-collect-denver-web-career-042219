e014b926d51347248a3b0413aaf9b3410443dc64
Joe Tustin <j.tustin@gmail.com>
2019-03-15 20:39:33 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -5,12 +5,12 @@ my_collect_var = Array.new
 
 def my_collect(collection) do
   i = 0
-
+  my_collect_var
   while i < collection.length
     yield
 
     i = i + 1
   end
   return my_collect_var
-end 
+end
 end
